8f6c6c3cb1785b7227500d93e9a45a7875018168
camille2003 <>
2022-04-06 20:20:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#Camille Pereira Sant'Anna
+
+ap1 = float(input("Digite a nota da Ap1"))
+
+ap2 = float(input("Digite a nota da Ap2"))
+
+ac = float(input("Digite a nota da Ac"))
+
+#Calcule a média do semestre da Camille
+
+ms = 0, 4 * (ap1 + ap2) + 0, 2 * ac
+
+#Nota do semestre da Camille
+
+print("A nota da Camille é",round(ms,1))
